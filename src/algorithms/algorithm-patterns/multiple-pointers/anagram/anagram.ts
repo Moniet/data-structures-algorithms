0ffa85e31a7ff9255b17949d70de63ac1cf3f35e
Moniet <moniet.sawhney@gmail.com>
2022-05-11 18:22:30 +0200

    anagram type fix

---
+++ src/algorithms/algorithm-patterns/multiple-pointers/anagram/anagram.ts
@@ -0,0 +1,29 @@
+type Letter = string
+interface CounterObj {
+  [index: string]: number
+}
+
+const anagram = (str1: string, str2: string) => {
+  const counterObj: CounterObj = {}
+
+  if (str1.length !== str2.length) return
+
+  for (let i: number = 0; i < str1.length; i++) {
+    const letter: Letter = str1[i]
+    counterObj[letter] ? counterObj[letter]++ : (counterObj[letter] = 1)
+  }
+
+  for (let j: number = 0; j < str2.length; j++) {
+    const letter = str2[j]
+
+    if (!counterObj[letter]) {
+      return false
+    } else {
+      counterObj[letter]--
+    }
+  }
+
+  return true
+}
+
+console.log(anagram("malayalam", "malayam"))
