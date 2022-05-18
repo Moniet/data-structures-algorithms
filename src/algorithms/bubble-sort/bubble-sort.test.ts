e8bd4a6930c629a4cdd9db3db9a47b65dbdc9dfa
Moniet <moniet.sawhney@gmail.com>
2022-05-18 18:23:46 +0200

    ✨ fix tests

--- src/algorithms/bubble-sort/bubble-sort.test.ts
+++ src/algorithms/bubble-sort/bubble-sort.test.ts
@@ -4,18 +4,12 @@ describe("Bubble Sort", () => {
   it("Sorts natural numbers correctly", () => {
     const arr = [6, 4, 2, 3, 1]
     const sortedArray = bubbleSort(arr)
-
-    expect(sortedArray[0]).toBe(1)
-    expect(sortedArray[1]).toBe(2)
-    expect(sortedArray[2]).toBe(3)
+    expect(sortedArray).toEqual([1, 2, 3, 4, 6])
   })
 
   it("Sorts integers correctly", () => {
     const arr = [2, 3, -6, -4, 2, 3, 1]
     const sortedArray = bubbleSort(arr)
-
-    expect(sortedArray[0]).toBe(-6)
-    expect(sortedArray[1]).toBe(-4)
-    expect(sortedArray[2]).toBe(1)
+    expect(sortedArray).toEqual([-6, -4, 1, 2, 2, 3, 3])
   })
 })
--- src/algorithms/insertion-sort/insertion-sort.test.ts
+++ src/algorithms/insertion-sort/insertion-sort.test.ts
@@ -2,20 +2,11 @@ import insertionSort from "./index"
 describe("Insersion Sort", () => {
   it("sorts whole numbers", () => {
     const sortedArr = insertionSort([3, 4, 5, 6, 1, 2, 0])
-
-    expect(sortedArr[0]).toBe(0)
-    expect(sortedArr[1]).toBe(1)
-    expect(sortedArr[2]).toBe(2)
+    expect(sortedArr).toEqual([0, 1, 2, 3, 4, 5, 6])
   })
 
   it("sorts integers", () => {
     const sortedArr = insertionSort([-2, -1, -3, 0, 1, 2])
-
-    expect(sortedArr[0]).toBe(-3)
-    expect(sortedArr[1]).toBe(-2)
-    expect(sortedArr[2]).toBe(-1)
-    expect(sortedArr[3]).toBe(0)
-    expect(sortedArr[4]).toBe(1)
-    expect(sortedArr[5]).toBe(2)
+    expect(sortedArr).toEqual([-3, -2, -1, 0, 1, 2])
   })
 })
