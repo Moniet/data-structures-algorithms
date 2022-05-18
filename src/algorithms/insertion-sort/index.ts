aae36c28222a43000e726591aa85b55485a54f37
Moniet <moniet.sawhney@gmail.com>
2022-05-18 18:16:55 +0200

    📝 insertion sort

---
+++ src/algorithms/insertion-sort/index.ts
@@ -0,0 +1,19 @@
+// Time complexity  : O(n^2)
+// Space complexity : O(1)
+
+const insertionSort = (arr: number[]) => {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i]
+    let j = i - 1
+
+    for (j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j]
+    }
+
+    arr[j + 1] = currentVal
+  }
+
+  return arr
+}
+
+export default insertionSort
--- src/algorithms/insertion-sort/insertion-sort.test.ts
+++ src/algorithms/insertion-sort/insertion-sort.test.ts
@@ -1,23 +1,21 @@
 import insertionSort from "./index"
 describe("Insersion Sort", () => {
-  it("Sorts whole", () => {
+  it("sorts whole numbers", () => {
     const sortedArr = insertionSort([3, 4, 5, 6, 1, 2, 0])
 
-    console.log(sortedArr)
-
-    expect(sortedArr?.[0]).toBe(0)
-    expect(sortedArr?.[1]).toBe(1)
-    expect(sortedArr?.[2]).toBe(2)
+    expect(sortedArr[0]).toBe(0)
+    expect(sortedArr[1]).toBe(1)
+    expect(sortedArr[2]).toBe(2)
   })
 
-  it("Sorts integers", () => {
-    const sortedArr = insertionSort([-2, -1, -3, 0, 1, 2, 3])
+  it("sorts integers", () => {
+    const sortedArr = insertionSort([-2, -1, -3, 0, 1, 2])
 
-    console.log(sortedArr)
-
-    expect(sortedArr?.[0]).toBe(-3)
-    expect(sortedArr?.[1]).toBe(-2)
-    expect(sortedArr?.[2]).toBe(-1)
-    expect(sortedArr?.[2]).toBe(0)
+    expect(sortedArr[0]).toBe(-3)
+    expect(sortedArr[1]).toBe(-2)
+    expect(sortedArr[2]).toBe(-1)
+    expect(sortedArr[3]).toBe(0)
+    expect(sortedArr[4]).toBe(1)
+    expect(sortedArr[5]).toBe(2)
   })
 })
